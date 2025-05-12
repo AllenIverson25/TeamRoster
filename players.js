cde3a54109833a30f0cd71ca907d0c600604aef8
AllenIverson25 <526jfriedman@frhsd.com>
2025-05-12 10:40:13 -0400

    first day doneeeeeeee

---
+++ players.js
@@ -0,0 +1,42 @@
+const players = [
+    {
+        firstName: "Francisco",
+        lastName: "Lindor",
+        position: "SS",
+        age: 31,
+        photo: "https://picsum.photos/200/300"
+    },
+    
+    {
+        firstName: "Pete",
+        lastName: "Alonso",
+        position: "1B",
+        age: 28,
+        photo: "https://picsum.photos/200/300",
+    },
+    {
+        firstName: "Brandon",
+        lastName: "Nimmo",
+        position: "CF",
+        age: 30,
+        photo: "https://picsum.photos/200/300",
+    },
+    {
+        firstName: "Jeff",
+        lastName: "McNeil",
+        position: "2B",
+        age: 31,
+        photo: "https://picsum.photos/200/300",
+    },
+    {
+        firstName: "Mark",
+        lastName: "Canha",
+        position: "RF",
+        age: 34,
+        photo: "https://picsum.photos/200/300"
+    }
+   
+]
+
+
+
