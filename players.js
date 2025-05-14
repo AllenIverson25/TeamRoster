06153314456508f1f794c30df308bd73f1fbc0bc
AllenIverson25 <526jfriedman@frhsd.com>
2025-05-14 09:24:58 -0400

    finished day one in class. fixed the roster and updated html

--- players.js
+++ players.js
@@ -1,42 +1,123 @@
+// Player data for the 2001-2002 Philadelphia 76ers
 const players = [
     {
-        firstName: "Francisco",
-        lastName: "Lindor",
-        position: "SS",
-        age: 31,
-        photo: "https://picsum.photos/200/300"
+        firstName: "Allen",
+        lastName: "Iverson",
+        position: "PG",
+        number: 3,
+        age: 26,
+        height: "6'0\"",
+        photo: "https://picsum.photos/id/237/400/500",
+        hiddenDetail: "League MVP for the 2000-2001 season, led the Sixers to the NBA Finals and averaged 31.1 points per game."
     },
-    
     {
-        firstName: "Pete",
-        lastName: "Alonso",
-        position: "1B",
+        firstName: "Dikembe",
+        lastName: "Mutombo",
+        position: "C",
+        number: 55,
+        age: 35,
+        height: "7'2\"",
+        photo: "https://picsum.photos/id/238/400/500",
+        hiddenDetail: "4-time NBA Defensive Player of the Year and known for his signature finger wag after blocking shots."
+    },
+    {
+        firstName: "Aaron",
+        lastName: "McKie",
+        position: "SG",
+        number: 8,
+        age: 29,
+        height: "6'5\"",
+        photo: "https://picsum.photos/id/239/400/500",
+        hiddenDetail: "Won the NBA Sixth Man of the Year Award for the 2000-2001 season."
+    },
+    {
+        firstName: "Eric",
+        lastName: "Snow",
+        position: "PG",
+        number: 20,
         age: 28,
-        photo: "https://picsum.photos/200/300",
+        height: "6'3\"",
+        photo: "https://picsum.photos/id/240/400/500",
+        hiddenDetail: "Known for his defensive skills and leadership on the court. He was an NBA All-Defensive Second Team selection."
     },
     {
-        firstName: "Brandon",
-        lastName: "Nimmo",
-        position: "CF",
-        age: 30,
-        photo: "https://picsum.photos/200/300",
-    },
-    {
-        firstName: "Jeff",
-        lastName: "McNeil",
-        position: "2B",
+        firstName: "George",
+        lastName: "Lynch",
+        position: "SF",
+        number: 9,
         age: 31,
-        photo: "https://picsum.photos/200/300",
+        height: "6'8\"",
+        photo: "https://picsum.photos/id/241/400/500",
+        hiddenDetail: "Known for his defensive prowess and rebounding ability."
     },
     {
-        firstName: "Mark",
-        lastName: "Canha",
-        position: "RF",
+        firstName: "Tyrone",
+        lastName: "Hill",
+        position: "PF",
+        number: 54,
+        age: 33,
+        height: "6'9\"",
+        photo: "https://picsum.photos/id/242/400/500",
+        hiddenDetail: "Was a key frontcourt contributor and made an All-Star appearance during his career."
+    },
+    {
+        firstName: "Matt",
+        lastName: "Geiger",
+        position: "C",
+        number: 52,
+        age: 32,
+        height: "7'0\"",
+        photo: "https://picsum.photos/id/243/400/500",
+        hiddenDetail: "Was one of the highest-paid centers in the NBA during his time with the Sixers."
+    },
+    {
+        firstName: "Speedy",
+        lastName: "Claxton",
+        position: "PG",
+        number: 5,
+        age: 23,
+        height: "5'11\"",
+        photo: "https://picsum.photos/id/244/400/500",
+        hiddenDetail: "Rookie in the 2001-2002 season who provided depth at the point guard position."
+    },
+    {
+        firstName: "Raja",
+        lastName: "Bell",
+        position: "SG",
+        number: 19,
+        age: 25,
+        height: "6'5\"",
+        photo: "https://picsum.photos/id/245/400/500",
+        hiddenDetail: "Known for his defensive skills and three-point shooting."
+    },
+    {
+        firstName: "Derrick",
+        lastName: "Coleman",
+        position: "PF",
+        number: 44,
         age: 34,
-        photo: "https://picsum.photos/200/300"
+        height: "6'10\"",
+        photo: "https://picsum.photos/id/246/400/500",
+        hiddenDetail: "Former #1 overall draft pick who provided veteran leadership to the team."
+    },
+    {
+        firstName: "Todd",
+        lastName: "MacCulloch",
+        position: "C",
+        number: 11,
+        age: 26,
+        height: "7'0\"",
+        photo: "https://picsum.photos/id/247/400/500",
+        hiddenDetail: "Canadian center who was known for his efficient shooting and soft touch around the basket."
+    },
+    {
+        firstName: "Corie",
+        lastName: "Blount",
+        position: "C",
+        number: 33,
+        age: 32,
+        height: "6'10\"",
+        photo: "https://picsum.photos/id/248/400/500",
+        hiddenDetail: "Veteran center who provided defensive depth in the frontcourt."
     }
-   
-]
-
-
-
+];
